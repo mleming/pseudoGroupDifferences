3c0f191557f58605b0c906af196270527fa23271
mleming <matthew.j.leming@gmail.com>
2015-02-21 14:47:29 -0500

    Fixed bug in sample script

--- output_noise_script.m
+++ output_noise_script.m
@@ -6,6 +6,7 @@ cz = [ 50 42 42 47 47 ];
 ellipsoid_radii = 4;
 FA_threshold = 0.01;
 L1_factor = 1.02;
+stddev = 0.005;
 pseudoGroupDifference('DTI_QCMI_005_1_DTI_float.nii', [cx' cy' cz']',...
       L1_factor, stddev, ellipsoid_radii, FA_threshold,...
       'output_noise.nii.gz');
